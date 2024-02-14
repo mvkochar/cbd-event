43132e08f45318ebf5db9355ac10d3953960016a
sgkochar <sgkochar@gmail.com>
2024-02-14 14:35:17 +0200

    add Services, Thematic, Video and News blocks to Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -115,6 +115,95 @@ const Home = ({ handlePage }: HomeProps) => {
         <div className='home-design-img'><img src="images/home-design.png" alt="home-design" /></div>
         <button type='button' className='home-design-more'>Детальніше</button>
       </div>
+      <div className="home-services">
+        <h2 className="home-services-title">Вибір послуг</h2>
+        <div className="home-services-box d-f jc-sb">
+          <div className="home-services-item">
+            <div className="services-item-bl">
+              <h3 className="services-item-title" style={{ marginLeft: "8px" }}>Логістика та реєстрація</h3>
+            </div>
+            <div className="services-item-bl d-f align-center">
+              <div className="services-item-line"></div>
+              <h3 className="services-item-title">Менеджмент призначення</h3>
+            </div>
+            <div className="services-item-bl d-f align-center">
+              <div className="services-item-line"></div>
+              <h3 className="services-item-title">Обговорення місця та об’єкта</h3>
+            </div>
+            <div className="services-item-bl d-f align-center">
+              <div className="services-item-line"></div>
+              <h3 className="services-item-title">Обслуговування</h3>
+            </div>
+          </div>
+          <div className="home-services-item">
+            <div className="services-item-bl d-f align-center">
+              <h3 className="services-item-title">Аудіовізуал</h3>
+              <div className="services-item-line"></div>
+            </div>
+            <div className="services-item-bl d-f align-center">
+              <h3 className="services-item-title">Звук і освітлення</h3>
+              <div className="services-item-line"></div>
+            </div>
+            <div className="services-item-bl d-f align-center">
+              <h3 className="services-item-title">Креативний дизайн і декор</h3>
+              <div className="services-item-line"></div>
+            </div>
+            <div className="services-item-bl d-f" style={{ marginRight: "8px" }}>
+              <h3 className="services-item-title">Фотографія та відеозйомка</h3>
+            </div>
+          </div>
+        </div>
+        <button className="home-services-know">Ознайомитися</button>
+      </div>
+      <div className="home-thematic">
+        <h2 className="home-thematic-title">
+          Тематичні події під ключ - <span>незабутні святкові емоції</span>
+        </h2>
+        <div className="home-thematic-box d-f jc-sb align-center">
+          <div>
+            <h3 className="thematic-info-title">Використовуємо найсучасніший дизайн</h3>
+            <p className="thematic-info-desc">
+              Ключ до чудової події — зробити її незабутньою. У CBD ми використовуємо концепцію
+              вашого бренду та особистості, щоб створювати настрій і встановлювати стандарти.
+            </p>
+            <p className="thematic-info-desc">
+              Тут НЕМАЄ подій, пов’язаних із розрізанням тортів. Кожен елемент дизайну налаштований 
+              на замовлення, і ми НІКОЛИ не ЗАБУВАЄМО дрібних деталей.
+            </p>
+            <button className='home-thematic-more'>Детальніше</button>
+          </div>
+          <div><img src="images/home-thematic.png" alt="home-thematic" /></div>
+        </div>
+      </div>
+      <div className="home-video">
+        <h2 className="home-video-title">Відео-тур</h2>
+        <div className="home-video-content">
+          <h3 className="video-content-title">Перегляд виконаних замовлень</h3>
+           <div className='video-content-img'><img src="images/video-content.png" alt="video-content" /></div>
+        </div>
+        <button className="home-video-khow">Ознайомитися</button>
+      </div>
+      <div className="home-news">
+        <h2 className="home-news-title">Останні новини</h2>
+        <div className="home-news-box d-f jc-sb">
+          <div className="home-news-item">
+            <div className="news-item-date">04 Травня 2019</div>
+            <div><img src="images/news1.png" alt="news1" /></div>
+            <h3 className="news-item-title">Як планується захід</h3>
+          </div>
+          <div className="home-news-item">
+            <div className="news-item-date">21 Серпня 2019</div>
+            <div><img src="images/news2.png" alt="news2" /></div>
+            <h3 className="news-item-title">Інформація для пошуку організатора</h3>
+          </div>
+          <div className="home-news-item">
+            <div className="news-item-date">28 Серпня 2019</div>
+            <div><img src="images/news3.png" alt="news3" /></div>
+            <h3 className="news-item-title">Зустрічі та заходи. Брошура</h3>
+          </div>
+        </div>
+        <button className='home-news-know'>Ознайомитися</button>
+      </div>
     </main>
   )
 }
